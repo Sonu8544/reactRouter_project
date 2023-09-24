b938266a19eb87ada83a6e3809ea5355638fb488
Sonu Kumar <sksonu8544@gmail.com>
2023-09-24 10:36:28 +0530

    Fetch Data Using Loader

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,20 +1,22 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Github = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/hiteshchoudhary")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+  const data = useLoaderData();
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/hiteshchoudhary")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     });
+  // }, []);
   return (
     <>
       <h1 className="text-center text-white bg-slate-600 text-3xl">
        
-        SONU Github Folowers :
+        SONU GitHub Followers :
         <span className="text-orange-600"> [ {data.followers} ]</span>
       </h1>
     </>
@@ -22,3 +24,8 @@ const Github = () => {
 };
 
 export default Github;
+
+export const githubInfoLoader = async () =>{
+  const response = await fetch("https://api.github.com/users/hiteshchoudhary")
+   return response.json();
+}
